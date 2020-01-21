9b132df07f0140308cbf6ee1c7a84dd9ba98a503
Michał Bartoszewicz <michal.bartoszewicz@grupawp.pl>
2020-01-21 17:13:29 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Joystick = require('joystick');
-const controller = (new Joystick)(0, 3500, 350);
+const controller = new Joystick(0, 3500, 350);
 
 controller.on('button', console.log);
 controller.on('axis', console.log);
